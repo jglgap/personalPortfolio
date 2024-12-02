5fe4112686a168483f20a96dc00727a4060d328b
jglgap <a23ricardosr@iessanclemente.net>
2024-12-02 10:01:49 +0000

    delete old stuff

--- portfolio_example/settings.py
+++ portfolio_example/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-07g)xbjibl@(*!prt*#gpey@m-i$rzqyn0@p8&ovu+#=ie1+xs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['maracuchuuu.pythonanywhere.com']
 
 
 # Application definition
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
